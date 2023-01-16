f2419326c6ad8a2ee9564532bb29c7b8559b36bd
domindez <109239601+domindez@users.noreply.github.com>
2023-01-16 14:39:35 +0100

    try

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,10 +3,19 @@
 import React from 'react';
 import '../sass/Home.scss';
 import TypingText from './TypingText';
+import Image from 'next/image';
+import profilePic from '../img/dani.jpeg';
 
 function Home() {
   return (    
     <div className={'home'}>
+      <Image className='profile-img'
+        src={profilePic}
+        alt='Picture of the author'
+        priority
+        width={400}
+        height={400}
+      />      
       <TypingText text='Hola, esto es una prueba...' />
     </div>    
   );
