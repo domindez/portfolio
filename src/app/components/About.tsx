26b0a88c45aecf06ae349ccdd7d9b9b4494537fb
domindez <109239601+domindez@users.noreply.github.com>
2024-05-24 12:44:27 +0200

    lorem

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,7 +3,10 @@ const About = () => {
     <section>
       <h3 className='section-title'>Sobre mí</h3>
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis beatae quis asperiores nobis odio saepe fugiat voluptatibus reprehenderit, ullam laborum quod, eos perferendis voluptates sit aliquid? Voluptates repellat harum fugit, ducimus ex molestias architecto quaerat earum similique repellendus delectus placeat maiores unde fuga hic mollitia accusamus sit, consequuntur nesciunt ipsam perspiciatis nihil. Harum quos id enim temporibus dolorem quaerat accusantium.</p>
+      <p>
+        Lorem fistrum diodeno sexuarl fistro al ataquerl. Benemeritaar va usté muy cargadoo sexuarl diodenoo al ataquerl papaar papaar. Pupita apetecan fistro diodeno por la gloria de mi madre diodenoo ese hombree a peich fistro te voy a borrar el cerito ese que llega. Se calle ustée pupita por la gloria de mi madre ahorarr. Ahorarr diodeno a gramenawer pecador amatomaa. Jarl amatomaa jarl papaar papaar diodeno jarl jarl a gramenawer quietooor fistro ahorarr. Sexuarl sexuarl te va a hasé pupitaa al ataquerl ese pedazo de está la cosa muy malar amatomaa ese que llega torpedo.
+      </p>
+
     </section>
   )
 }
