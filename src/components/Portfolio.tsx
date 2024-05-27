f774fb21f65248a866edea8bc66a017f513bbd15
domindez <109239601+domindez@users.noreply.github.com>
2024-05-27 09:35:03 +0200

    trabajos

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -13,7 +13,7 @@ const Portfolio = () => {
 
   return (
     <section className='portfolio' id='portfolio'>
-      <h3 className='section-title'>Proyectos</h3>
+      <h3 className='section-title'>Mis Trabajos</h3>
 
       <div className='project-list'>
         <PortfolioProject
